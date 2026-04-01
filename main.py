46a111460fbc9fc61f4838602f79794852011e7d
Usanogh-6 <milenanurijanyan@gmail.com>
2026-04-01 13:47:33 +0400

    Library_customer class

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import matplotlib as plt
+import numpy as np
+import csv
+
 class Libcustomer:
     def __init__(self, name, surname, ):
 
+class Library_customer:
+    def __init__(self, name, surname, date, title):
+        self.name = name
+        self.surname = surname
+        self.date = date
+        self.title = title
 
 
-
-
+    def
 
